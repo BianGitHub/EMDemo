eef703191b09313fa79ec3e70519773ef326e642
边雷 <bianlei@csii.com.cn>
2017-07-04 16:51:12 +0800

    导入SDK / 配置 / 登录注册

--- EMDemo/EMDemo/BLLoginController.m
+++ EMDemo/EMDemo/BLLoginController.m
@@ -7,6 +7,7 @@
 //
 
 #import "BLLoginController.h"
+#import <EaseMob.h>
 
 @interface BLLoginController ()
 @property (weak, nonatomic) IBOutlet UITextField *username;
@@ -22,9 +23,35 @@
     
 }
 - (IBAction)LoginClick:(UIButton *)sender {
+    
+    if (self.username.text.length == 0 || self.password.text.length == 0) {
+        NSLog(@"请输入正确的用户名或密码");
+        return;
+    }
+    [[EaseMob sharedInstance].chatManager asyncLoginWithUsername:self.username.text password:self.password.text completion:^(NSDictionary *loginInfo, EMError *error) {
+        
+        if (error) {
+            NSLog(@"登录失败 - %@", error);
+        } else {
+            NSLog(@"登录成功 - %@", loginInfo);
+        }
+    } onQueue:nil];
 }
 
 - (IBAction)registerClick:(UIButton *)sender {
+    
+    if (self.username.text.length == 0 || self.password.text.length == 0) {
+        NSLog(@"请输入正确的用户名或密码");
+        return;
+    }
+    [[EaseMob sharedInstance].chatManager asyncRegisterNewAccount:self.username.text password:self.password.text withCompletion:^(NSString *username, NSString *password, EMError *error) {
+       
+        if (error) {
+            NSLog(@"注册失败 - %@", error);
+        } else {
+            NSLog(@"注册成功");
+        }
+    } onQueue:nil];
 }
 
 @end
